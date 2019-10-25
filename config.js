e85150cf4ad1da745765a8cf20719b415b670fbb
LCY <cunyizju@126.com>
2019-10-25 10:16:30 +0800

    sql

--- config.js
+++ config.js
@@ -17,38 +17,10 @@ module.exports = {
         // Required scope for the client-side viewer
         public: ['viewables:read']
     },
-<<<<<<< HEAD
     sqlconnection : {
-        host: '192.168.1.12',
-=======
-    sqlconnection: {
         host: '47.110.84.253',
->>>>>>> 3c664732247d87160d6c8e55036b89cc39bd49ba
         user: 'hctech',
         password: 'hc123',
-        database: 'ZemenBank201908'
+        database: 'zemenbank'
     }
-<<<<<<< HEAD
-    // sqlconnection : {
-    //     host: 'localhost',
-    //     user: 'hctech',
-    //     password: 'hc123',
-    //     database: 'zemenbank'
-    // }
 };
-=======
-};
-
-function chooseClientId(username) {
-    switch (username) {
-        case 'cxn2':
-            ForgeAll.FORGE_CLIENT_ID = "oyMP7fHNIHXrGfdxnsSeJ9p7o1HlJz4m";
-            ForgeAll.FORGE_CLIENT_SECRET = "nA8YA1KcR5OjkDrG";
-            break;
-        case 'cxn1':
-            ForgeAll.FORGE_CLIENT_ID = "EAImUtWq2VNlA3VAA7ZBWYdJ2KLZksiU";
-            ForgeAll.FORGE_CLIENT_SECRET = "zzXT3Ix7dt76Bprd";
-            break;
-    }
-}
->>>>>>> 3c664732247d87160d6c8e55036b89cc39bd49ba
